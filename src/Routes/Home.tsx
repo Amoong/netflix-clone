cfe01649709f133f35276bfaf3911e000a6c0460
MuYoungJeong <moo1323@naver.com>
2021-12-26 21:21:01 +0900

    Add slider

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,8 +1,133 @@
+import { getMovies, IGetMoviesResult } from "api";
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
+import { useQuery } from "react-query";
+import styled from "styled-components";
+import { makeImagePath } from "utils";
+
+const Wrapper = styled.div`
+  background: black;
+`;
+
+const Loader = styled.div`
+  height: 20vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Banner = styled.div<{ bgPhoto: string }>`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 100vh;
+  padding: 60px;
+  background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)),
+    url(${props => props.bgPhoto});
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center center;
+`;
+
+const Title = styled.h2`
+  font-size: 68px;
+  margin-bottom: 20px;
+`;
+
+const Overview = styled.p`
+  font-size: 36px;
+  width: 50%;
+`;
+
+const Slider = styled.div`
+  position: relative;
+  top: -100px;
+`;
+
+const Row = styled(motion.div)`
+  display: grid;
+  gap: 5px;
+  grid-template-columns: repeat(6, 1fr);
+  margin-bottom: 5px;
+  position: absolute;
+  width: 100%;
+`;
+
+const Box = styled(motion.div)<{ bgPhoto: string }>`
+  background-color: teal;
+  background-image: url(${props => props.bgPhoto});
+  background-size: cover;
+  background-position: center center;
+  font-size: 64px;
+  height: 200px;
+`;
+
+const rowVariants = {
+  hidden: {
+    x: window.innerWidth - 5,
+  },
+  visible: {
+    x: 0,
+  },
+  exit: {
+    x: -(window.innerWidth - 5),
+  },
+};
+
+const offset = 6;
+
 function Home() {
+  const { data, isLoading } = useQuery<IGetMoviesResult>(["movies", "nowPlaying"], getMovies);
+
+  const [index, setIndex] = useState(0);
+  const [leaving, setLeaving] = useState(false);
+  const increaseIndex = () => {
+    if (data) {
+      if (leaving) return;
+      toggleLeaving();
+      const totalMovies = data.results.length;
+      const maxIndex = Math.floor(totalMovies / offset);
+      setIndex(prev => (prev === maxIndex ? 0 : prev + 1));
+    }
+  };
+
+  const toggleLeaving = () => setLeaving(prev => !prev);
+
   return (
-    <div style={{ backgroundColor: "rgba(0, 0, 0, 0.8)", height: "200vh" }}>
-      <h1>Home</h1>
-    </div>
+    <Wrapper>
+      {isLoading ? (
+        <Loader>Loading</Loader>
+      ) : (
+        <>
+          <Banner
+            onClick={increaseIndex}
+            bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}
+          >
+            <Title>{data?.results[0].title}</Title>
+            <Overview>{data?.results[0].overview}</Overview>
+          </Banner>
+          <Slider>
+            <AnimatePresence initial={false} onExitComplete={toggleLeaving}>
+              <Row
+                variants={rowVariants}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+                transition={{ type: "tween", duration: 1 }}
+                key={index}
+              >
+                {data?.results
+                  .slice(1)
+                  .slice(offset * index, offset * index + offset)
+                  .map(movie => (
+                    <Box key={movie.id} bgPhoto={makeImagePath(movie?.backdrop_path, "w500")}></Box>
+                  ))}
+              </Row>
+            </AnimatePresence>
+          </Slider>
+        </>
+      )}
+    </Wrapper>
   );
 }
 
---
+++ src/api.ts
@@ -0,0 +1,36 @@
+const API_KEY = "3f575e78f34a21a926b1534c85924795";
+const BASE_PATH = "https://api.themoviedb.org/3";
+
+interface IMovie {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: Date;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+export interface IGetMoviesResult {
+  dates: {
+    maximum: string;
+    minimum: string;
+  };
+  page: number;
+  results: IMovie[];
+  total_pages: number;
+  total_results: number;
+}
+
+export function getMovies() {
+  return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(response =>
+    response.json(),
+  );
+}
