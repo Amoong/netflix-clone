ba6e65d1578eaed2665f41ec6ecb0e059426cc24
MuYoungJeong <moo1323@naver.com>
2021-12-26 11:06:10 +0900

    Add Header

---
+++ src/Routes/Home.tsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div style={{ backgroundColor: "rgba(0, 0, 0, 0.8)", height: "200vh" }}>
+      <h1>Home</h1>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/Routes/Tv.tsx
@@ -0,0 +1,9 @@
+function Tv() {
+  return (
+    <div style={{ backgroundColor: "rgba(0, 0, 0, 0.8)", height: "100vh" }}>
+      <h1>Tv</h1>;
+    </div>
+  );
+}
+
+export default Tv;
